f7e9cecd781452eb5ab7391a29df2c755b9af82d
Felix <felix.urtubia@gmail.com>
2020-10-21 15:21:34 -0300

    Fourth commit

--- script1.sh
+++ script1.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
-# Check our system
+set -e
 
-echo "Memory: "
-free -h
-echo "Disk Usage: "
-du -h
-echo "Uptime: "
-Uptime 
+showtime() {
+	echo "Showing time"
+	TIME ="$(date)"
+	echo $TIME 
+}
+
+leave() {
+	echo"______________________"
+	echo"_  UPDATE COMPLETE! __"
+	echo"______________________"
+	exit
+}
+
+script1-help() {
+cat << _EOF_
+This is a tool that executes script1 :D. Nothing else to say.
+
+_EOF_
+
+}
+
+if [ "$1" == "--help" ]; then
+	script1-help
+	exit
+fi
+
+if [ "$1" == "--showtime" ]; then
+	showtime
+	leave
+fi
+
 
 exit
